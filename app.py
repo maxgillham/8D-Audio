ebb54c550b936ae62068c00cfd0586f1a7e99057
Max Gillham <14mfg2@queensu.ca>
2018-10-19 13:24:49 -0400

    can now upload and save in sample_audio, convert to 8D and save in sample_output

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import *
-from werkzeug.utils import secure_filename
+from audio_features import song_features, rotate_left_right, add_effects, save_song
 import os
 
 
@@ -7,50 +7,42 @@ app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+def convert_to_8D():
+    os.chdir(APP_ROOT + '/sample_audio')
+    file_name = os.listdir()
+
+    wav_mono, wav_stereo, sampling_rate, tempo, beat_frame = song_features(file_name[0])
+    wav = rotate_left_right(wav_mono, wav_stereo, tempo, sampling_rate)
+
+    os.chdir(APP_ROOT + '/sample_output')
+    save_song('in_1.wav', wav, sampling_rate)
+    add_effects('in_1.wav')
+    return
+
+
 #homepage
 @app.route('/')
 def index():
    return render_template('index.html')
 
-@app.route("/upload", methods=["POST"])
+@app.route('/upload', methods=['POST'])
 def upload():
-    target = os.path.join(APP_ROOT, 'songs/')
-    print(target)
+    target = os.path.join(APP_ROOT, 'sample_audio/')
+
     if not os.path.isdir(target):
         os.mkdir(target)
-    print(request.files.getlist("file"))
-    for upload in request.files.getlist("file"):
-        print(upload)
-        print("{} is the file name".format(upload.filename))
-        filename = upload.filename
-        # This is to verify files are supported
-        ext = os.path.splitext(filename)[1]
-        if (ext == ".wav") or (ext == ".mp3"):
-            print("File supported moving on...")
-        else:
-            render_template("Error.html", message="Files uploaded are not supported...")
-        destination = "/".join([target, filename])
-        print("Accept incoming file:", filename)
-        print("Save it to:", destination)
-        upload.save(destination)
 
-    # return send_from_directory("images", filename, as_attachment=True)
-    return
+    for file in request.files.getlist('file'):
+        filename = file.filename
+        destination = '/'.join([target, filename])
+        file.save(destination)
+    convert_to_8D()
+    return render_template('index.html')
 
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
-
-@app.route('/static/')
-def download_file(filename):
-    return send_file('/static/effectz.wav', filename)
-
-
-
-
-
+@app.route('/sample_output/')
+def download_file():
+    return send_file('/sample_output/effectz.wav')
 
 if __name__ == '__main__':
    app.run(debug = True)
