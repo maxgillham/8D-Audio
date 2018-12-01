8415fbc6f508ee2885d1532509e1d74e0e8e52e5
maxgillham <maxfgillham@gmail.com>
2018-12-01 11:15:47 -0500

    removed upload picker

--- app.py
+++ app.py
@@ -16,23 +16,13 @@ TEMPLATES_AUTO_RELOAD = True
 def convert_to_8D():
     os.chdir(APP_ROOT + '/sample_audio')
     file_name = os.listdir()
-
     wav_mono, wav_stereo, sampling_rate, tempo, beat_frame = song_features(file_name[0])
     wav_mono_elevated = elevation(wav_mono, tempo, sampling_rate)
     wav = rotate_left_right(wav_mono_elevated, wav_stereo, tempo, sampling_rate)
-    #l = elevation(wav[0,:], tempo, sampling_rate)
-    #r = elevation(wav[1,:], tempo, sampling_rate)
-    #y = np.stack((l,r))
     os.chdir(APP_ROOT + '/static')
-<<<<<<< HEAD
-    save_song('test.wav', y, sampling_rate)
-    add_effects('test.wav')
-    return
-=======
     save_song('in.wav', wav, sampling_rate)
     add_effects('in.wav')
     return 
->>>>>>> 2aecd565318a9afe13ae40c4907649114e63fe38
 
 def clear_directories():
     os.chdir(APP_ROOT + '/sample_audio')
@@ -42,7 +32,6 @@ def clear_directories():
     os.chdir(APP_ROOT + '/static')
     files = os.listdir()
     for file in files:
-        print(file)
         os.remove(file)
     os.chdir(APP_ROOT)
     return
@@ -58,30 +47,6 @@ def maybe_make_dir():
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    print('\nHIT UPLOAD')
-    target = os.path.join(APP_ROOT, 'sample_audio/')
-
-    if not os.path.isdir(target):
-        os.mkdir(target)
-
-    for file in request.files.getlist('file'):
-        filename = 'test.wav'
-        destination = '/'.join([target, filename])
-        file.save(destination)
-
-    convert_to_8D()
- 
-    
-    #render_template('listen.html')
-    return render_template('index.html')
-
-@app.route('/reset')
-def reset():
-    return render_template('index.html')
-
-
 @app.route('/static/effectz.wav')
 def download_file():
     return send_file(APP_ROOT+ '/static/effectz.wav')
@@ -95,12 +60,7 @@ def download_by_link():
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    #maybe_make_dir()
-    TEMPLATES_AUTO_RELOAD = True
-=======
     maybe_make_dir()
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
->>>>>>> 2aecd565318a9afe13ae40c4907649114e63fe38
     app.run(debug = True)
