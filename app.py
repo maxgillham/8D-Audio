3419073bad0c0e358e85cba37f64e9259ecb51ee
maxgillham <maxfgillham@gmail.com>
2018-12-16 20:34:25 -0500

    got rid of pointless directory changing

--- app.py
+++ app.py
@@ -12,21 +12,27 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 TEMPLATES_AUTO_RELOAD = True
 
 
-
+#when called turns the file in sample audio into 8d
 def convert_to_8D():
-    os.chdir(APP_ROOT + '/sample_audio')
-    file_name = os.listdir()
-    wav_mono, wav_stereo, sampling_rate, tempo, beat_frame = song_features(file_name[0])
+    #os.chdir(APP_ROOT + '/sample_audio')
+    #file_name = os.listdir()
+    #load song as time series
+    wav_mono, wav_stereo, sampling_rate, tempo, beat_frame = song_features('./static/test.wav')
+    #elevate mono signal
     wav_mono_elevated = elevation(wav_mono, tempo, sampling_rate)
+    #rotate stereo panning based off elevated signal
     wav = rotate_left_right(wav_mono_elevated, wav_stereo, tempo, sampling_rate)
-    os.chdir(APP_ROOT + '/static')
-    save_song('in.wav', wav, sampling_rate)
-    add_effects('in.wav')
-    return 
+    #os.chdir(APP_ROOT + '/static')
+    #save before SoX adds effects
+    save_song('./static/in.wav', wav, sampling_rate)
+    #apply SoX transformer
+    add_effects('./static/in.wav')
+    return
 
+#wipe the previous songs, had issues when overwriting and webpage not properally reloading
 def clear_directories():
-    if os.path.exists(APP_ROOT + '/sample_audio/test.wav'):
-        os.remove(APP_ROOT + '/sample_audio/test.wav')
+    if os.path.exists(APP_ROOT + '/static/test.wav'):
+        os.remove(APP_ROOT + '/static/test.wav')
     if os.path.exists(APP_ROOT + '/static/effectz.wav'):
         os.remove(APP_ROOT + '/static/effectz.wav')
     if os.path.exists(APP_ROOT + '/static/in.wav'):
@@ -35,7 +41,7 @@ def clear_directories():
 
 def maybe_make_dir():
     files = os.listdir()
-    if 'sample_audio' not in files: os.mkdir('sample_audio')
+    #if 'sample_audio' not in files: os.mkdir('sample_audio')
     if 'static' not in files: os.mkdir('static')
     return
 
@@ -45,15 +51,22 @@ def index():
     os.chdir(APP_ROOT)
     return render_template('index.html')
 
+#to request to download song to local machine
 @app.route('/static/effectz.wav')
 def download_file():
     return send_file(APP_ROOT+ '/static/effectz.wav')
 
+#route for when link is submitted
 @app.route('/download_by_link', methods=['POST'])
 def download_by_link():
+    #clear directories of previous songs
     clear_directories()
+    os.chdir(APP_ROOT)
+    #downlaod the audio from the link given
     download_from_youtube(request.values['link'])
+    #convert downloaded song in sample audio to 8d and save in static
     convert_to_8D()
+    #render listening page with audio controller setup to play the file saved by convert to 8d
     return render_template('listen.html')
 
 
